80ec7afecae0c93bd8e1b4e610aafc7ef0dd8daf
jjmuchai <jjmuchai151@gmail.com>
2025-08-01 14:24:35 +0300

    chore: ignore .env to protect secrets

--- vite.config.js
+++ vite.config.js
@@ -2,34 +2,35 @@ import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 import path from 'path'
 
+// https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+
   resolve: {
     alias: {
       '@': path.resolve(__dirname, './src'),
       '@components': path.resolve(__dirname, './src/components'),
-      '@pages': path.resolve(__dirname, './src/pages'),
-      '@hooks': path.resolve(__dirname, './src/hooks'),
       '@services': path.resolve(__dirname, './src/services'),
-      '@store': path.resolve(__dirname, './src/store'),
-      '@utils': path.resolve(__dirname, './src/utils'),
-      '@constants': path.resolve(__dirname, './src/constants'),
+      '@pages': path.resolve(__dirname, './src/pages'),
       '@assets': path.resolve(__dirname, './src/assets'),
-      '@styles': path.resolve(__dirname, './src/styles'),
-    },
+      '@utils': path.resolve(__dirname, './src/utils')
+    }
   },
+
   server: {
     port: 5173,
     proxy: {
       '/api': {
-        target: 'http://localhost:5000',
+        target: 'http://localhost:5000', // Local Flask backend for dev
         changeOrigin: true,
-        rewrite: (path) => path.replace(/^\/api/, ''),
-      },
-    },
+        rewrite: path => path.replace(/^\/api/, '')
+      }
+    }
   },
+
   build: {
     outDir: 'dist',
     sourcemap: true,
-  },
+    chunkSizeWarningLimit: 1000 // Silence large chunk warning (~500kb default)
+  }
 })
